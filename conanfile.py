428c8edb1f70e7464cee375f195c586ada3f9ace
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-01-06 11:22:56 +0300

    Updated CMakeLists.txt files and conanfile.py file for Conan 2.0 compatibility.

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,30 @@
-from conans import ConanFile, CMake, tools
+from os.path import join
+
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.files import copy
 
 
 class DataGeneratorConan(ConanFile):
-    name = "DataGenerator-C"
+    name = "data_generator_c"
     version = "1.0.0"
-    requires = ["AnnotatedTree-C/1.0.0", "Classification-C/1.0.0"]
+    requires = ["annotated_tree_c/1.0.0",
+                "classification_c/1.0.0",
+                "dictionary_c/1.0.0",
+                "morphological_analysis_c/1.0.0",
+                "util_c/1.0.0",
+                "data_structure_c/1.0.0",
+                "named_entity_recognition_c/1.0.0",
+                "annotated_sentence_c/1.0.0",
+                "corpus_c/1.0.0",
+                "frame_net_c/1.0.0",
+                "prop_bank_c/1.0.0",
+                "xml_parser_c/1.0.0",
+                "dependency_parser_c/1.0.0",
+                "senti_net_c/1.0.0",
+                "math_c/1.0.0",
+                "sampling_c/1.0.0",
+                "parse_tree_c/1.0.0"]
     license = "GPL License"
     author = "Olcay Taner Yildiz olcay.yildiz@ozyegin.edu.tr"
     url = "https://github.com/StarlangSoftware/DataGenerator-C"
@@ -13,25 +33,28 @@ class DataGeneratorConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    generators = "cmake"
     exports_sources = "src/*", "Test/*"
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="src")
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include")
-        self.copy("*DataGenerator.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        copy("*.h", keep_path=False, src=join(self.source_folder), dst=join(self.package_folder, "include"), pattern="*.h")
+        copy("*.a", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.a")
+        copy("*.so", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.so")
+        copy("*.dylib", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.dylib")
+        copy("*.dll", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "bin"), pattern="*.dll")
 
     def package_info(self):
         self.cpp_info.libs = ["DataGenerator"]
